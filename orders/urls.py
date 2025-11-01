2a0fa1849394bee543035e73c65b848172bd53a8
luigi-ichi <159853434+luigi-ichi@users.noreply.github.com>
2025-11-01 16:46:25 +0800

    feat: hardcoded routing of past order details and order success page

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('order/', views.create_order, name='create_order'),
+    path('order_details/<int:order_id>/', views.past_order_details, name='order_details'),
+    path('order-success/', views.order_success, name='order_success'),
 ]
