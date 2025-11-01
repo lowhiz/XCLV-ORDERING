51a1cec0bab1c6cce803be5ee262cc75b96c2afb
luigi-ichi <159853434+luigi-ichi@users.noreply.github.com>
2025-11-01 16:46:51 +0800

    feat: hardcoded routing to view individual table details

--- menu/urls.py
+++ menu/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('view/', views.view_menu, name='view_menu'),
     path('review/', views.order_review, name='order_review'),
+    path('table-details/', views.table_details, name='table_details'),
 ]
