c0cfad5b1bfe82ce5072db3d28505eae887834cd
luigi-ichi <159853434+luigi-ichi@users.noreply.github.com>
2025-12-03 17:11:36 +0800

    fix: description attribute of Item model changed to TextField to accomodate longer descriptions

--- menu/models.py
+++ menu/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 class Item(models.Model):
     """
-    Model representing a single item or product available for order 
+    Model representing a single item or product available for order
     This model serves as the core inventory or price list reference.
     """
-    
+
     # Classification for grouping similar items.
     category = models.CharField(max_length=255)
-    
+
     # The common, display name of the item.
     name = models.CharField(max_length=255)
-    
+
     # An explanation or detailing of the item.
-    description = models.CharField(max_length=255)
-    
-    # The selling price of one unit of this item. 
+    description = models.TextField()
+
+    # The selling price of one unit of this item.
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
