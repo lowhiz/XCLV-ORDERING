e39daa9023bb6ab9afa15c862aa06183ec23337e
luigi-ichi <159853434+luigi-ichi@users.noreply.github.com>
2026-05-06 16:04:47 +0800

    feat: add link endpoint of toggling item availability to be used in JS

--- menu/urls.py
+++ menu/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('open_menu/', views.open_menu, name='open_menu'),
     path('admin_toggle_menu/', views.admin_toggle_menu, name='admin_toggle_menu'),
     path('check_menu_status/', views.check_menu_status, name='check_menu_status'),
+    # Feature armed from the DRF inventory app to allow admins to toggle item availability from the admin menu page
+    path('toggle-availability/<int:item_id>/', views.toggle_item_availability, name='toggle_item_availability'),
 ]
