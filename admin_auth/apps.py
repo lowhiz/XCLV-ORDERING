3f4f22b1dacf11532542a7261544e082ebd9cbf6
luigi-ichi <159853434+luigi-ichi@users.noreply.github.com>
2026-05-27 15:21:43 +0800

    feat: remove signals.py since admin credentials and menu are no longer needed to be preloaded to the database
    feat: remove references to deleted signals.py in their apps.py

--- admin_auth/apps.py
+++ admin_auth/apps.py
@@ -2,7 +2,3 @@ from django.apps import AppConfig
 class AdminAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'admin_auth'
-    
-    # To initialize the fixtures
-    def ready(self):
-        from . import signals
--- menu/apps.py
+++ menu/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 class MenuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'menu'
-
-    def ready(self):
-        from . import signals 
